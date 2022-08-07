d27fcb9cb980792e66a9ea575853d04c540e6497
Mathias Klenk <mathiasklenk@gmx.de>
2022-08-07 14:51:06 +0200

    Back to JS version

--- src/lib/server/validate.ts
+++ src/lib/server/validate.ts
@@ -2,20 +2,16 @@ import { validationResult } from 'express-validator';
 
 const validate = (validations) => {
   return async (req, res, next) => {
-    if (validations) {
-      await Promise.all(validations.map((validation) => validation.run(req)));
-      const errors = validationResult(req);
+    await Promise.all(validations.map((validation) => validation.run(req)));
+    const errors = validationResult(req);
 
-      if (errors.isEmpty()) {
-        return next();
-      }
-
-      const errorObject = {};
-      errors.array().forEach((error) => (errorObject[error.param] = error));
-      res.status(422).json({ errors: errorObject });
-    } else {
-      res.status(422).json({ errors: 'Error validating' });
+    if (errors.isEmpty()) {
+      return next();
     }
+
+    const errorObject = {};
+    errors.array().forEach((error) => (errorObject[error.param] = error));
+    res.status(422).json({ errors: errorObject });
   };
 };
 
