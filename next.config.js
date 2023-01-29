a95455f3003bb0617f676b9b12d4221e6f1d8d4e
Anders Gerner <github@andersgerner.dk>
2023-01-29 08:52:41 +0100

    Add possibility to analyze bundle

--- next.config.js
+++ next.config.js
@@ -4,10 +4,14 @@
 
 const { i18n } = require('./next-i18next.config.js')
 
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   i18n,
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
   reactStrictMode: true,
-}
+})
